6de7a089e6dd44015e592fb8203fcc76aa3b973d
Soji Yamakawa <soji@andrew.cmu.edu>
2024-04-29 14:02:00 -0400

    TEST.C

---
+++ FMT_DOS/COMMAND/TEST.C
@@ -0,0 +1,12 @@
+/*
+Size of TEST.COM
+Empty main -> 1070 bytes
+_dos_open  -> 1318 bytes
+fopen      -> 6886 bytes
+printf     -> 8524 bytes
+*/
+#include <stdio.h>
+int main(void)
+{
+	return 0;
+}
